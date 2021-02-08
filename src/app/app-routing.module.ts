a5285966b2542e86c21e20ecfa535e8df6a14959
Ashok Kumar K <kak.mca@hotmail.com>
2021-02-08 10:34:57 +0530

    New programs added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/departments', pathMatch: 'full' },
-  { path: 'departments', component: DepartmentListComponent },
+  { path: 'departments', component: DepartmentListComponent, canActivate: [AuthGuard] },
   { path: 'departments/:id', component: DepartmentDetailComponent },
   { path: 'employees', component: EmployeeListComponent },
+  { path: 'feedback', component: FeedbackComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
 export const routingComponents = [
   DepartmentListComponent,
   EmployeeListComponent,
+  FeedbackComponent,
   PageNotFoundComponent,
   DepartmentDetailComponent
 ];
