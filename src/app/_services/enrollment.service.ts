55718e1b4cd4d2705ae2dd71223eab1b955fdc22
Ashok Kumar K <kak.mca@hotmail.com>
2021-02-08 10:34:57 +0530

    New programs added.

---
+++ src/app/_services/enrollment.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../_models/user';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnrollmentService {
+  // Change the api controller name to some invalid string in
+  // the below URL to see the error message 
+  url = 'http://localhost:55495/api/angularusers';
+  constructor(private http: HttpClient) { }
+
+  enroll(user: User) {
+    return this.http.post<any>(this.url, user)
+              .pipe(catchError(this.errorHandler)); 
+  }
+
+  errorHandler(error: HttpErrorResponse) {
+    return throwError(error);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BootstrapPracticeComponent } from './bootstrap-practice/bootstrap-practice.component';
 import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EnableButtonComponent } from './enable-button/enable-button.component';
 import { ClickExampleComponent } from './click-example/click-example.component';
 import { InputEventDemoComponent } from './input-event-demo/input-event-demo.component';
@@ -24,6 +24,8 @@ import { GetEmployeesFromJsonComponent } from './get-employees-from-json/get-emp
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 import { FormsExample1Component } from './forms-example1/forms-example1.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 // import { DepartmentListComponent } from './department-list/department-list.component';
 // import { EmployeeListComponent } from './employee-list/employee-list.component';
@@ -52,15 +54,19 @@ import { FormsExample1Component } from './forms-example1/forms-example1.componen
     routingComponents,
     PageNotFoundComponent,
     DepartmentDetailComponent,
-    FormsExample1Component
+    FormsExample1Component,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
-    //AppRoutingModule,
+    AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
